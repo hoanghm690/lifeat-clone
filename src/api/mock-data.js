91eb4822f4821467d543f58be03c87aa8fa0a514
Hoang Huynh <hoanghuynh0609@gmail.com>
2023-03-16 18:10:43 +0700

    mock data fortune

--- src/api/mock-data.js
+++ src/api/mock-data.js
@@ -140,3 +140,15 @@ export const videos = [
     categoryId: 3
   }
 ]
+
+export const fortunes = [
+  {
+    id: 1,
+    description:
+      '"The secret of happiness is not in doing what one likes, but in liking what one does."'
+  },
+  {
+    id: 2,
+    description: '"The more one judges, the less one loves."'
+  }
+]
