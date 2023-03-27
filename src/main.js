a19006e6a1b62c9bc53b554a03f75cdb1b383fef
Hoang Huynh <hoanghuynh0609@gmail.com>
2023-03-27 21:11:25 +0700

    remove unuse code

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 import './assets/global.css'
 
 const app = createApp(App)
 
 app.use(router)
-
-app.use(store)
 
 app.mount('#app')
